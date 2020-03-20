7ef2597fa7ab981123bd862db34822711c0655ac
josvieira <josienevieira20@gmail.com>
2020-03-20 17:08:08 -0300

    Criação dos filtros

--- src/main/java/com/errorscentral/guiabolso/service/ErrorService.java
+++ src/main/java/com/errorscentral/guiabolso/service/ErrorService.java
@@ -11,7 +11,7 @@ import com.errorscentral.guiabolso.repository.ErrorRepository;
 
 @Service
 public class ErrorService {
-    
+
     @Autowired
     private ErrorRepository errorRepository;
 
@@ -22,12 +22,34 @@ public class ErrorService {
     public Error updateError(Error error){
         return errorRepository.save(error);
     }
-    
+
     public List<Error> findAllErros(){
     	return errorRepository.findAll();
-    	
+
     }
     public Error add(Error error) {
 		return errorRepository.save(error);
 	}
+
+    public List<Error> listErrorFilter(String environment, String level, String event) {
+        if (!environment.equals("null") && !level.equals("null") && !event.equals("null")) {
+            return errorRepository.findByLevelAndEventAndEnvironment(level, event, environment);
+        } else if (!environment.equals("null") && !level.equals("null")) {
+            return errorRepository.findByLevelAndEnvironment(level, environment);
+        } else if (!environment.equals("null") && !event.equals("null")) {
+            return errorRepository.findByEventAndEnvironment(event, environment);
+        } else if(!level.equals("null") && !event.equals("null")) {
+            return errorRepository.findByLevelAndEvent(level, event);
+        }else if(!environment.equals("null")) {
+            return errorRepository.findByEnvironment(environment);
+        }else if (!event.equals("null")){
+            return errorRepository.findByEvent(event);
+        }else{
+            return errorRepository.findByLevel(level);
+        }
+    }
+
+    public List<Error> listErrorDescription(String description){
+        return errorRepository.findByDetailsLogContainingIgnoreCase(description);
+    }
 }
